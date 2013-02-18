87f9b45e5a34f4826dba43db5ecb2b5c6cdefd88
luis cabarique <luis.cabarique@laspartes.com.co>
2013-02-18 11:29:15 -0500

    Versioón 3 de laspartes libraries

---
+++ application/libraries/phpexcel2.php
@@ -0,0 +1,12 @@
+<?php 
+
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+include dirname(__FILE__) . '/Classes/PHPExcel.php';
+
+class phpexcel{
+	function __construct(){
+	}
+}
+
+?>
